5e0f06de68080ef2dbc18221e659a2ce41a2b6d9
Enrique Arranz <enrique.arranz@urjc.es>
2017-04-11 12:37:28 +0200

    initial

---
+++ src/main/java/com/miw/eps/MiwApplication.java
@@ -0,0 +1,12 @@
+package com.miw.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiwApplication.class, args);
+	}
+}
